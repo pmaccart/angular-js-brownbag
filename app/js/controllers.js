4bbe9166c2fc4a06f2731e0fa5e1ae9d7ed4e00a
pmaccart <pmaccart@alumni.nd.edu>
2012-09-21 02:30:25 -0500

    load messages via $http service

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,24 +3,26 @@
 /* Controllers */
 
 
-function InboxCtrl($scope) {
+function InboxCtrl($scope, $http) {
 
     $scope.username = "Phil";
-    $scope.messages = [
-        {id: 1, checked: false, status: "unread", from: "Studio Mail Server",subject: "Commit Build Failed", date: new Date(2012, 9, 15, 9, 12, 14)},
-        {id: 2, checked: false, status: "unread", from: "Studio Mail Server",subject: "Commit Build Failed", date: new Date(2012, 9, 15, 9, 28, 22)},
-        {id: 3, checked: false, status: "unread", from: "Studio Mail Server",subject: "Commit Build Failed", date: new Date(2012, 9, 15, 10, 5, 40)},
-        {id: 4, checked: false, status: "unread", from: "Studio Mail Server",subject: "Commit Build Failed", date: new Date(2012, 9, 15, 9, 53, 40)},
-        {id: 5, checked: false, status: "unread", from: "Liz Tobin", subject: "Enter Your Time", date: new Date(2012, 9, 15, 11, 34, 19)},
-        {id: 6, checked: false, status: "unread", from: "Anthony Lee", subject: "Enter JIRA Hours", date: new Date(2012, 9, 15, 7, 45, 0)},
-        {id: 7, checked: false, status: "unread", from: "Tom Snapp", subject: "Feeding the Bulldog", date: new Date(2012, 9, 15, 12, 32, 45)}
-    ];
-
     $scope.sortFields = [
         "status", "from", "subject", "date"
     ];
-
     $scope.sortBy = "date";
 
+    $http({
+        method:'GET',
+        url:'data/messages.json'
+    }).success(function (data) {
+            console.log(data);
+            data.forEach(function(msg) {
+                if (msg.date) msg.date =  new Date(Date.parse(msg.date));
+            });
+            $scope.messages = data;
+        }).error(function (data, status) {
+            alert("Oops! Status = " + status);
+        });
+
 }
-InboxCtrl.$inject = ["$scope"];
+InboxCtrl.$inject = ["$scope", "$http"];
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -3,13 +3,32 @@
 /* jasmine specs for controllers go here */
 
 describe('InboxCtrl', function () {
-    it('should create a  model with the username and messages', function () {
-        var scope = {},
-            ctrl = new InboxCtrl(scope);
+    var messages, scope, ctrl, $httpBackend;
 
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+        $httpBackend = _$httpBackend_;
+        messages = [
+            {id: 3, from: "Sender 3", subject: "Subject 3", date: "2000-01-03"},
+            {id: 2, from: "Sender 2", subject: "Subject 2", date: "2000-01-02"},
+            {id: 4, from: "Sender 4", subject: "Subject 4", date: "2000-01-04"},
+            {id: 1, from: "Sender 1", subject: "Subject 1", date: "2000-01-01"}
+        ];
+
+        $httpBackend.expectGET('data/messages.json').respond(messages);
+
+        scope = $rootScope.$new();
+        ctrl = $controller(InboxCtrl, {$scope: scope});
+    }))
+
+    it('should create a  model with the username and sort by field', function () {
         expect(scope.username).toBe('Phil');
-        expect(scope.messages.length).toBe(7);
         expect(scope.sortBy).toBe('date');
     });
+
+    it('should fetch messages from data/messages.json', function () {
+        expect(scope.messages).toBeUndefined();
+        $httpBackend.flush();
+        expect(scope.message).toEqual(messages);
+    })
 });
 
