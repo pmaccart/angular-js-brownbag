134a19d7fa3b8cfd8486372c39922794230e2378
pmaccart <pmaccart@alumni.nd.edu>
2012-09-21 01:09:40 -0500

    add messages, repeater; update unit tests

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,6 +6,15 @@
 function InboxCtrl($scope) {
 
     $scope.username = "Phil";
+    $scope.messages = [
+        {checked: false, status: "unread", from: "Studio Mail Server",subject: "Commit Build Failed", date: new Date(2012, 9, 15, 9, 12, 14)},
+        {checked: false, status: "unread", from: "Studio Mail Server",subject: "Commit Build Failed", date: new Date(2012, 9, 15, 9, 28, 22)},
+        {checked: false, status: "unread", from: "Studio Mail Server",subject: "Commit Build Failed", date: new Date(2012, 9, 15, 10, 5, 40)},
+        {checked: false, status: "unread", from: "Studio Mail Server",subject: "Commit Build Failed", date: new Date(2012, 9, 15, 9, 53, 40)},
+        {checked: false, status: "unread", from: "Liz Tobin", subject: "Enter Your Time", date: new Date(2012, 9, 15, 11, 34, 19)},
+        {checked: false, status: "unread", from: "Anthony Lee", subject: "Enter JIRA Hours", date: new Date(2012, 9, 15, 7, 45, 0)},
+        {checked: false, status: "unread", from: "Tom Snapp", subject: "Feeding the Bulldog", date: new Date(2012, 9, 15, 12, 32, 45)}
+    ];
 
 }
 InboxCtrl.$inject = ["$scope"];
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -3,10 +3,12 @@
 /* jasmine specs for controllers go here */
 
 describe('InboxCtrl', function () {
-    it('should create a username model with the user name', function () {
+    it('should create a  model with the username and messages', function () {
         var scope = {},
             ctrl = new InboxCtrl(scope);
 
         expect(scope.username).toBe('Phil');
+        expect(scope.messages.length).toBe(7);
     });
 });
+
