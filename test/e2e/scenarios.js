d61aa4b5696d671d0e40d1f3620675773a9bc022
pmaccart <pmaccart@alumni.nd.edu>
2012-09-21 01:10:34 -0500

    add empty scenario test

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,17 +2,14 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
-describe('Angular JS Brownbag App', function() {
+describe('Angular JS Brownbag App', function () {
 
-  beforeEach(function() {
-    browser().navigateTo('../../app/index.html');
-  });
+    beforeEach(function () {
+        browser().navigateTo('../../app/index.html');
+    });
 
+    it('should do something...', function () {
 
-  it('should display a hello message when the page is loaded', function() {
-    expect(element("body.h2")).toBe("Hello Slalom Custom AppDev!");
-
-  });
-
+    });
 
 });
