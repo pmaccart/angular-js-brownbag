9d557d8c4bd87fd78aa0361a848636fb80dbbeac
pmaccart <pmaccart@alumni.nd.edu>
2012-09-20 22:53:05 -0500

    setup step 1

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,44 +2,17 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
-describe('my app', function() {
+describe('Angular JS Brownbag App', function() {
 
   beforeEach(function() {
     browser().navigateTo('../../app/index.html');
   });
 
 
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    expect(browser().location().url()).toBe("/view1");
-  });
-
-
-  describe('view1', function() {
-
-    beforeEach(function() {
-      browser().navigateTo('#/view1');
-    });
-
-
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element('[ng-view] p:first').text()).
-        toMatch(/partial for view 1/);
-    });
+  it('should display a hello message when the page is loaded', function() {
+    expect(element("body.h2")).toBe("Hello Slalom Custom AppDev!");
 
   });
 
 
-  describe('view2', function() {
-
-    beforeEach(function() {
-      browser().navigateTo('#/view2');
-    });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element('[ng-view] p:first').text()).
-        toMatch(/partial for view 2/);
-    });
-
-  });
 });
